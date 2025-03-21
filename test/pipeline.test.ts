9fb1accade25b6dfc8c649f4eb89f47f72d4355c
nishant345 <nishant.gaurav345@gmail.com>
2025-03-21 14:01:13 +0530

    Add test to pipeline.

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -3,10 +3,11 @@ import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as Pipeline from '../lib/pipeline-stack';
 
 
-test('Empty Stack', () => {
+test('Pipeline Stack', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
   // THEN
-  Template.fromStack(stack).hasResourceProperties("AWS::CodePipeline::Pipeline", {})
+  // Template.fromStack(stack).hasResourceProperties("AWS::CodePipeline::Pipeline", {})
+  expect(Template.fromStack(stack).toJSON()).toMatchSnapshot()
 });
