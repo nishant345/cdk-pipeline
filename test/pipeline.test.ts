48355e4e62a91a80a426b00bd9212b07b8586113
nishant345 <nishant.gaurav345@gmail.com>
2025-03-21 16:44:49 +0530

    Add test for addserviceStage of pipelineStack

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as Pipeline from '../lib/pipeline-stack';
+import { ServiceStack } from '../lib/service-stack';
+
 
 
 test('Pipeline Stack', () => {
@@ -11,3 +13,18 @@ test('Pipeline Stack', () => {
   // Template.fromStack(stack).hasResourceProperties("AWS::CodePipeline::Pipeline", {})
   expect(Template.fromStack(stack).toJSON()).toMatchSnapshot()
 });
+
+test('Adding Service Stage', () => {
+  const app = new cdk.App();
+  const pipeline = new Pipeline.PipelineStack(app, 'PipelineStack');
+  const service = new ServiceStack(app, 'ServiceStack');
+
+  pipeline.addServiceStage(service, 'Test');
+
+  Template.fromStack(pipeline).hasResourceProperties("AWS::CodePipeline::Pipeline", {
+    Stages: Match.arrayWith([
+        Match.objectLike({Name: 'Test'})
+        ]
+    )
+  })
+})
