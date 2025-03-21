d9f28ea1f6ef2a693a3191edb24affbb5335ba8d
nishant345 <nishant.gaurav345@gmail.com>
2025-03-21 13:40:21 +0530

    Add test to pipeline.

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -2,16 +2,11 @@ import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as Pipeline from '../lib/pipeline-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
+
+test('Empty Stack', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
   // THEN
-
-  const template = Template.fromStack(stack);
-
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+  Template.fromStack(stack).hasResourceProperties("AWS::CodePipeline::Pipeline", {})
 });
